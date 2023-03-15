a0b6ee078935666f5509af7c630ca477b04180fa
Ben King <benking1293@gmail.com>
2023-03-15 16:17:26 +0000

    Added button to start again

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import Header from '../components/Header'
 
 export default function Result() {
   return (
     <div>
         <Header />
+        <Link to="/">
+            <button>Try again</button>
+        </Link>
         
         {/* if form completes successfully then show button to change password
             if form is unsuccessful then show button to start again  */}
