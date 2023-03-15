4dadb04b0d54ba2daa14462cb6a17673687008c0
Ben King <benking1293@gmail.com>
2023-03-15 17:59:15 +0000

    Fetching data from hook and putting into state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import PasswordRecovery from "./pages/passwordRecovery";
 import Home from "./pages/home";
 import Result from "./pages/result";
+import fetchData from "./hooks/api";
 
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const fetchedData = await fetchData()
+      setData(fetchedData)
+      console.log(fetchedData)
+    }
+    getData()
+    
+  }, [])
+
+  console.log(data.user1)
+
+  
+
   return (
     <BrowserRouter>
       <Routes>
