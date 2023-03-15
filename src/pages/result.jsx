67b8326837c2eb5abc48b1c566008cb284df4fa1
Ben King <benking1293@gmail.com>
2023-03-15 13:03:25 +0000

    result page

---
+++ src/pages/result.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Result() {
+  return (
+    <div>
+        <Header />
+        
+        {/* if form completes successfully then show button to change password
+            if form is unsuccessful then show button to start again  */}
+    </div>
+  )
+}
