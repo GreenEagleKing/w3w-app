aec1f5373607e736192274efd8d8d6e26e6b6235
Ben King <benking1293@gmail.com>
2023-04-06 18:54:52 +0100

    basic styling for result page

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -11,40 +11,50 @@ export default function Result({
 }) {
   return (
     <div className="pageWrapper">
-      <Header />
-      {isNewUser && isCreated ? (
-        <div>
-          <p>Locations successfully created for xxxx user.</p>
+      <section className="container">
+        <Header />
+        <div className="result-container">
+          {isNewUser && isCreated ? (
+            <div>
+              <p>Locations successfully created for xxxx user.</p>
+            </div>
+          ) : isUpdating ? (
+            <div>
+              <p>Locations successfully updated for xxxx user.</p>
+            </div>
+          ) : isNewUser && !isCreated ? (
+            <div>
+              <p>Locations unsuccessfully created. Please try again.</p>
+              <div className="button-wrapper">
+                <Link to="/setLocations">
+                  <button className="bn30">Go Back</button>
+                </Link>
+              </div>
+            </div>
+          ) : isLocation ? (
+            <div>
+              <p>Locations successfully matched. Please retrieve password.</p>
+              <Link to="/">
+                <button className="bn30">Retrieve Password</button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <p>Locations did not match. Please try again.</p>
+              <div className="button-wrapper">
+                <Link to="/">
+                  <button className="bn30">Try Again</button>
+                </Link>
+              </div>
+            </div>
+          )}
+          <span>
+            <Link to="/">
+              <button className="bn30">Home</button>
+            </Link>
+          </span>
         </div>
-      ) : isUpdating ? (
-        <div>
-          <p>Locations successfully updated for xxxx user.</p>
-        </div>
-      ) : isNewUser && !isCreated ? (
-        <div>
-          <p>Locations unsuccessfully created. Please try again.</p>
-          <Link to="/setLocations">
-            <button>Go Back</button>
-          </Link>
-        </div>
-      ) : isLocation ? (
-        <div>
-          <p>Locations successfully matched. Please retrieve password.</p>
-          <Link to="/">
-            <button>Retrieve Password</button>
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <p>Locations did not match. Please try again.</p>
-          <Link to="/">
-            <button>Try Again</button>
-          </Link>
-        </div>
-      )}
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+      </section>
     </div>
   )
 }
