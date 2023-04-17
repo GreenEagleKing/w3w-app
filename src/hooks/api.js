ef6da629e9909fac720cb11887b83206d97e3421
Ben King <benking1293@gmail.com>
2023-04-17 11:41:29 +0100

    Removed old API connection

--- src/hooks/api.js
+++
@@ -1,7 +0,0 @@
-
-// Fetching mock user data for testing using postman api
-export default async function fetchData() {
-  const response = await fetch("https://99a59328-7f3c-4be4-a9a1-0e6b55a2902b.mock.pstmn.io/get")
-  const data = await response.json()
-  return data
-}
