cdacbe8cd0928ee29a68d81872146aae4c6df41d
Ben King <benking1293@gmail.com>
2023-05-12 17:53:08 +0100

    New position on page and collapsable

--- src/components/HintMessage.jsx
+++ src/components/HintMessage.jsx
@@ -3,26 +3,34 @@ import React from "react"
 export default function HintMessage({ currentUser }) {
   return (
     <div className="hint-container container">
-      <h4>MVP Tester/Helper</h4>
-      <p>
-        User data is displayed here to make testing the functionality easier.
-      </p>
-      {currentUser && (
-        <div className="hint-data">
+      <input id="collapsible" class="toggle" type="checkbox" />
+      <label for="collapsible" class="lbl-toggle">
+        MVP Helper
+      </label>
+      <div class="collapsible-content">
+        <div class="content-inner">
           <p>
-            <b>Current User: </b>
-            {currentUser.username}
+            User data is displayed here to make testing the functionality
+            easier.
           </p>
-          <p>
-            <b>Location 1: </b>
-            {currentUser.what3wordLocations.locationOne}
-          </p>
-          <p>
-            <b>Location 2: </b>
-            {currentUser.what3wordLocations.locationTwo}
-          </p>
+          {currentUser && (
+            <div className="hint-data">
+              <p>
+                <b>Current User: </b>
+                {currentUser.username}
+              </p>
+              <p>
+                <b>Location 1: </b>
+                {currentUser.what3wordLocations.locationOne}
+              </p>
+              <p>
+                <b>Location 2: </b>
+                {currentUser.what3wordLocations.locationTwo}
+              </p>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   )
 }
