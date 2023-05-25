0428ce50bea9ef0755818d81f226b1cfce644e29
Ben King <benking1293@gmail.com>
2023-05-25 11:15:17 +0100

    Updated styling details and content

--- src/pages/setLocations.jsx
+++ src/pages/setLocations.jsx
@@ -23,27 +23,38 @@ export default function SetLocations({
       <section className="container">
         <Header />
         <div className="create-user-container">
-          <p className="p-centered">
-            Please create a new username or find existing to set new locations.
-          </p>
           <div className="create-user-wrapper">
-            <RetrieveLocation
-              findUser={findUser}
-              handleIsRetrieving={handleIsRetrieving}
-            />
-            <CreateUser
-              createUser={createUser}
-              isNewUser={isNewUser}
-              findUser={findUser}
-              handleIsNewUser={handleIsNewUser}
-              handleIsCreated={handleIsCreated}
-            />
-            <CheckUser
-              findUser={findUser}
-              createUser={createUser}
-              handleUpdateUser={handleUpdateUser}
-              handleIsCreated={handleIsCreated}
-            />
+            <div className="retrieve-location-wrapper">
+              <p className="p-centered">
+                Retrieve password if you have already setup your locations
+              </p>
+              <RetrieveLocation
+                findUser={findUser}
+                handleIsRetrieving={handleIsRetrieving}
+              />
+            </div>
+
+            <div className="horizontal-line"></div>
+
+            <div className="newUpdate-location-wrapper">
+              <p id="newUpdate-p" className="p-centered">
+                Or create a new username or find an existing user to update
+                locations
+              </p>
+              <CreateUser
+                createUser={createUser}
+                isNewUser={isNewUser}
+                findUser={findUser}
+                handleIsNewUser={handleIsNewUser}
+                handleIsCreated={handleIsCreated}
+              />
+              <CheckUser
+                findUser={findUser}
+                createUser={createUser}
+                handleUpdateUser={handleUpdateUser}
+                handleIsCreated={handleIsCreated}
+              />
+            </div>
           </div>
         </div>
       </section>
