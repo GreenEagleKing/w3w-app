e4a5bbd4194c8929052adb26fa17903904e5b7b1
Ben King <benking1293@gmail.com>
2023-06-01 13:14:04 +0100

    Updated hint box CSS

--- src/components/HintMessage.jsx
+++ src/components/HintMessage.jsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 
 export default function HintMessage({ currentUser }) {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleContainer = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  const containerStyle = {
+    maxWidth: isExpanded ? "500px" : "240px",
+    maxHeight: isExpanded ? "350px" : "70px",
+  }
+
   return (
-    <div id="hint-container" className="container">
-      <input id="collapsible" className="toggle" type="checkbox" />
+    <div id="hint-container" className="container" style={containerStyle}>
+      <input
+        id="collapsible"
+        className="toggle"
+        type="checkbox"
+        checked={isExpanded}
+        onChange={toggleContainer}
+      />
       <label htmlFor="collapsible" className="lbl-toggle">
         MVP Helper
       </label>
@@ -18,7 +35,7 @@ export default function HintMessage({ currentUser }) {
           </div>
           <p className="p-centered">
             Current user data is displayed here to help make testing the app
-            functionality easier.
+            functionality easier. Copy locations into the map search bar.
           </p>
           {currentUser && (
             <div className="hint-data">
